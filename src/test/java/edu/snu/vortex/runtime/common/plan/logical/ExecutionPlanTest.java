fc66c8c9945625f7eea67802b65356e53af29f41
Joo Yeon Kim <jykim88@gmail.com>
2017-03-22 16:19:27 +0900

    PhysicalPlanTest added

--- src/test/java/edu/snu/vortex/runtime/common/plan/logical/ExecutionPlanTest.java
+++ src/test/java/edu/snu/vortex/runtime/common/plan/logical/ExecutionPlanTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.snu.vortex.runtime.common.plan;
+package edu.snu.vortex.runtime.common.plan.logical;
 
 import edu.snu.vortex.compiler.ir.*;
 import edu.snu.vortex.runtime.common.plan.logical.ExecutionPlan;
