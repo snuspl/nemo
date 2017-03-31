f35e997acc5490aea3eb2148730a7e121a72bf53
Minhyeok Kweun <kmh910924@gmail.com>
2017-03-31 12:09:33 +0900

    move ChannelSelector into channel package

--- src/main/java/edu/snu/vortex/runtime/executor/channel/ChannelSelector.java
+++ src/main/java/edu/snu/vortex/runtime/executor/channel/ChannelSelector.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.snu.vortex.runtime.executor;
+package edu.snu.vortex.runtime.executor.channel;
 
 
 import edu.snu.vortex.compiler.ir.Element;
