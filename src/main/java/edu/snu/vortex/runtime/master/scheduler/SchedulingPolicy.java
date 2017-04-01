c59621d36f406cf73609c6ae1880c84cd6d8b66a
Joo Yeon Kim <jykim88@gmail.com>
2017-04-01 23:38:51 +0900

    SchedulingPolicy: Assumptions added

--- src/main/java/edu/snu/vortex/runtime/master/scheduler/SchedulingPolicy.java
+++ src/main/java/edu/snu/vortex/runtime/master/scheduler/SchedulingPolicy.java
@@ -26,19 +26,64 @@ import java.util.Optional;
 interface SchedulingPolicy {
 
   /**
+   * Returns this scheduling policy's timeout before an executor assignment.
+   * @return the timeout in milliseconds.
+   */
+  long getScheduleTimeout();
+
+  /**
    * Attempts to schedule the given taskGroup to an executor according to this policy.
+   * If there is no executor available for the taskGroup, it waits for an executor to be assigned before it times out.
+   * (Depending on the executor's resource type)
+   *
    * @param taskGroup to schedule
    * @return executorId on which the taskGroup is scheduled if successful, an empty Optional otherwise.
    */
   Optional<String> attemptSchedule(final TaskGroup taskGroup);
 
+  /**
+   * Adds the executor to the pool of available executors.
+   * Unlocks this policy to schedule a next taskGroup if locked.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor that has been added.
+   */
   void onExecutorAdded(final ExecutorRepresenter executor);
 
+  /**
+   * Deletes the executor from the pool of available executors.
+   * Locks this policy from scheduling if there is no more executor currently available for the next taskGroup.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor that has been deleted.
+   */
   void onExecutorDeleted(final ExecutorRepresenter executor);
 
+  /**
+   * Marks the executor scheduled for the taskGroup.
+   * Locks this policy from scheduling if there is no more executor currently available for the next taskGroup.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor assigned for the taskGroup.
+   * @param taskGroup scheduled to the executor.
+   */
   void onTaskGroupScheduled(final ExecutorRepresenter executor, final TaskGroup taskGroup);
 
+  /**
+   * Tentative.
+   * @param executor .
+   * @param taskGroup .
+   */
   void onTaskGroupLaunched(final ExecutorRepresenter executor, final TaskGroup taskGroup);
 
-  void onTaskGroupExecutionComplete(final ExecutorRepresenter executor, final String taskGroupId);
+  /**
+   * Marks the taskGroup's completion in the executor.
+   * Unlocks this policy to schedule a next taskGroup if locked.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor where the taskGroup's execution has completed.
+   * @param taskGroup whose execution has completed.
+   */
+
+  void onTaskGroupExecutionComplete(final ExecutorRepresenter executor, final TaskGroup taskGroup);
 }
