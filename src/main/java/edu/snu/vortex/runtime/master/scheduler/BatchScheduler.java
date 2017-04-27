c4ca49da585773c085b5ef7a3e2a38671a46a2a8
Joo Yeon Kim <jykim88@gmail.com>
2017-04-27 17:02:46 +0900

    Scheduler -> BatchScheduler

---
+++ src/main/java/edu/snu/vortex/runtime/master/scheduler/BatchScheduler.java
@@ -0,0 +1,269 @@
+/*
+ * Copyright (C) 2017 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.vortex.runtime.master.scheduler;
+
+import edu.snu.vortex.runtime.common.RuntimeAttribute;
+import edu.snu.vortex.runtime.common.comm.ExecutorMessage;
+import edu.snu.vortex.runtime.common.plan.physical.PhysicalPlan;
+import edu.snu.vortex.runtime.common.plan.physical.PhysicalStage;
+import edu.snu.vortex.runtime.common.plan.physical.TaskGroup;
+import edu.snu.vortex.runtime.common.state.JobState;
+import edu.snu.vortex.runtime.common.state.StageState;
+import edu.snu.vortex.runtime.common.state.TaskGroupState;
+import edu.snu.vortex.runtime.exception.IllegalStateTransitionException;
+import edu.snu.vortex.runtime.exception.SchedulingException;
+import edu.snu.vortex.runtime.exception.UnknownExecutionStateException;
+import edu.snu.vortex.runtime.exception.UnrecoverableFailureException;
+import edu.snu.vortex.runtime.master.ExecutionStateManager;
+import edu.snu.vortex.runtime.master.ExecutorRepresenter;
+
+import java.util.*;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Scheduler receives a {@link PhysicalPlan} to execute and asynchronously schedules the task groups.
+ * The policy by which it schedules them is dependent on the implementation of {@link SchedulingPolicy}.
+ */
+public final class Scheduler {
+  private static final Logger LOG = Logger.getLogger(Scheduler.class.getName());
+
+  private final ExecutorService schedulerThread;
+  private final BlockingDeque<TaskGroup> taskGroupsToSchedule;
+  private final ExecutionStateManager executionStateManager;
+
+  /**
+   * A map of executor ID to the corresponding {@link ExecutorRepresenter}.
+   * This object is synchronized as multiple threads can access and modify {@link ExecutorRepresenter}s.
+   */
+  private final Map<String, ExecutorRepresenter> executorRepresenterMap;
+
+  /**
+   * The {@link SchedulingPolicy} used to schedule task groups.
+   * {@link this#schedulingPolicyAttribute} decides the implementation.
+   */
+  private SchedulingPolicy schedulingPolicy;
+  private RuntimeAttribute schedulingPolicyAttribute;
+
+  /**
+   * Timeout for the {@link SchedulingPolicy} upon task group scheduling.
+   */
+  private long scheduleTimeout;
+
+  /**
+   * The list of stages for the currently running job.
+   * This object is synchronized as a new job can be submitted while a job runs.
+   */
+  private List<PhysicalStage> physicalStages;
+  private PhysicalStage currentStage;
+
+  public Scheduler(final ExecutionStateManager executionStateManager,
+                   final RuntimeAttribute schedulingPolicyAttribute,
+                   final long scheduleTimeout) {
+    this.executionStateManager = executionStateManager;
+
+    this.schedulerThread = Executors.newSingleThreadExecutor();
+    this.taskGroupsToSchedule = new LinkedBlockingDeque<>();
+    this.executorRepresenterMap = new HashMap<>();
+    schedulerThread.execute(new TaskGroupScheduleHandler());
+
+    // The default policy is initialized and set here.
+    this.schedulingPolicyAttribute = schedulingPolicyAttribute;
+    initializeSchedulingPolicy();
+
+    this.scheduleTimeout = scheduleTimeout;
+    this.physicalStages = Collections.emptyList();
+  }
+
+  /**
+   * Receives a job to schedule.
+   * @param physicalPlan the physical plan for the job.
+   */
+  public void scheduleJob(final PhysicalPlan physicalPlan) {
+    synchronized (physicalStages) {
+      this.physicalStages = physicalPlan.getStageDAG().getTopologicalSort();
+    }
+    initializeSchedulingPolicy();
+    executionStateManager.manageNewJob(physicalPlan);
+    scheduleNextStage();
+  }
+
+  /**
+   * Receives a {@link edu.snu.vortex.runtime.common.comm.ExecutorMessage.TaskGroupStateChangedMsg} from an executor.
+   * The message is received via communicator where this method is called.
+   * @param executorId the id of the executor where the message was sent from.
+   * @param message from the executor.
+   */
+  // TODO #83: Introduce Task Group Executor
+  // TODO #94: Implement Distributed Communicator
+  public void onTaskGroupStateChanged(final String executorId,
+                                      final ExecutorMessage.TaskGroupStateChangedMsg message) {
+    final TaskGroupState.State newState = convertState(message.getState());
+    final boolean currentStageComplete =
+        executionStateManager.onTaskGroupStateChanged(message.getTaskGroupId(), newState);
+    switch (newState) {
+    case COMPLETE:
+      synchronized (executorRepresenterMap) {
+        onTaskGroupExecutionComplete(executorRepresenterMap.get(executorId),
+            message.getTaskGroupId(), currentStageComplete);
+      }
+      break;
+    case FAILED_RECOVERABLE:
+      synchronized (executorRepresenterMap) {
+        onTaskGroupExecutionFailed(executorRepresenterMap.get(executorId), message.getTaskGroupId(),
+            message.getFailedTaskIdsList());
+      }
+      break;
+    case FAILED_UNRECOVERABLE:
+      throw new UnrecoverableFailureException(new Exception(new StringBuffer().append("The job failed on TaskGroup #")
+          .append(message.getTaskGroupId()).append(" in Executor ").append(executorId).toString()));
+    case READY:
+    case EXECUTING:
+      throw new IllegalStateTransitionException(new Exception("The states READY/EXECUTING cannot occur at this point"));
+    default:
+      throw new UnknownExecutionStateException(new Exception("This TaskGroupState is unknown: " + newState));
+    }
+  }
+
+  private void onTaskGroupExecutionComplete(final ExecutorRepresenter executor,
+                                            final String taskGroupId,
+                                            final boolean currentStageComplete) {
+    schedulingPolicy.onTaskGroupExecutionComplete(executor, taskGroupId);
+
+    // if the current stage is complete,
+    if (currentStageComplete) {
+      final boolean jobComplete =
+          executionStateManager.onStageStateChanged(currentStage.getId(), StageState.State.COMPLETE);
+
+
+      if (jobComplete) { // and if the job is complete,
+        executionStateManager.onJobStateChanged(JobState.State.COMPLETE);
+      } else { // and if the job is still executing, schedule the next stage
+        scheduleNextStage();
+      }
+    }
+  }
+
+  // TODO #163: Handle Fault Tolerance
+  private void onTaskGroupExecutionFailed(final ExecutorRepresenter executor, final String taskGroupId,
+                                          final List<String> taskIdOnFailure) {
+    schedulingPolicy.onTaskGroupExecutionFailed(executor, taskGroupId);
+  }
+
+  // TODO #85: Introduce Resource Manager
+  public void onExecutorAdded(final ExecutorRepresenter executor) {
+    schedulingPolicy.onExecutorAdded(executor);
+    synchronized (executorRepresenterMap) {
+      executorRepresenterMap.put(executor.getExecutorId(), executor);
+    }
+  }
+
+  // TODO #163: Handle Fault Tolerance
+  // TODO #85: Introduce Resource Manager
+  public void onExecutorRemoved(final ExecutorRepresenter executor) {
+    synchronized (executorRepresenterMap) {
+      executorRepresenterMap.remove(executor.getExecutorId());
+    }
+    final Set<String> taskGroupsToReschedule = schedulingPolicy.onExecutorRemoved(executor);
+
+    // Reschedule taskGroupsToReschedule
+  }
+
+  /**
+   * Schedules the next stage to execute.
+   * It adds the list of task groups for the stage where the scheduler thread continuously polls from.
+   */
+  private synchronized void scheduleNextStage() {
+    synchronized (physicalStages) {
+      currentStage = physicalStages.remove(0);
+    }
+    taskGroupsToSchedule.addAll(currentStage.getTaskGroupList());
+    executionStateManager.onStageStateChanged(currentStage.getId(), StageState.State.EXECUTING);
+  }
+
+  /**
+   * Initializes the scheduling policy.
+   * This can be called anytime during this scheduler's lifetime and the policy will change flexibly.
+   */
+  private void initializeSchedulingPolicy() {
+    switch (schedulingPolicyAttribute) {
+    case Batch:
+      this.schedulingPolicy = new RoundRobinSchedulingPolicy(scheduleTimeout);
+      break;
+    default:
+      throw new SchedulingException(new Exception("The scheduling policy is unsupported by runtime"));
+    }
+  }
+
+  /**
+   * A separate thread is run to schedule task groups to executors.
+   */
+  private class TaskGroupScheduleHandler implements Runnable {
+    @Override
+    public void run() {
+      while (!schedulerThread.isShutdown()) {
+        try {
+          final TaskGroup taskGroup = taskGroupsToSchedule.takeFirst();
+          final Optional<ExecutorRepresenter> executor = schedulingPolicy.attemptSchedule(taskGroup);
+          System.out.println(taskGroup);
+          System.out.println(executor);
+          if (!executor.isPresent()) {
+            LOG.log(Level.INFO, "Failed to assign an executor before the timeout: {0}",
+                schedulingPolicy.getScheduleTimeout());
+            taskGroupsToSchedule.addLast(taskGroup);
+          } else {
+            // TODO #83: Introduce Task Group Executor
+            // TODO #94: Implement Distributed Communicator
+            // Must send this taskGroup to the destination executor.
+            schedulingPolicy.onTaskGroupScheduled(executor.get(), taskGroup.getTaskGroupId());
+            System.out.println("Scheduling! " + taskGroup.getTaskGroupId());
+            executionStateManager.onTaskGroupStateChanged(taskGroup.getTaskGroupId(),
+                TaskGroupState.State.EXECUTING);
+          }
+        } catch (final Exception e) {
+          throw new SchedulingException(e);
+        }
+      }
+    }
+  }
+
+  public void terminate() {
+    schedulerThread.shutdown();
+    taskGroupsToSchedule.clear();
+  }
+
+  // TODO #164: Cleanup Protobuf Usage
+  private TaskGroupState.State convertState(final ExecutorMessage.TaskGroupStateFromExecutor state) {
+    switch (state) {
+    case READY:
+      return TaskGroupState.State.READY;
+    case EXECUTING:
+      return TaskGroupState.State.EXECUTING;
+    case COMPLETE:
+      return TaskGroupState.State.COMPLETE;
+    case FAILED_RECOVERABLE:
+      return TaskGroupState.State.FAILED_RECOVERABLE;
+    case FAILED_UNRECOVERABLE:
+      return TaskGroupState.State.FAILED_UNRECOVERABLE;
+    default:
+      throw new UnknownExecutionStateException(new Exception("This TaskGroupState is unknown: " + state));
+    }
+  }
+}
--- src/main/java/edu/snu/vortex/runtime/master/scheduler/Scheduler.java
+++ src/main/java/edu/snu/vortex/runtime/master/scheduler/Scheduler.java
@@ -15,255 +15,81 @@
  */
 package edu.snu.vortex.runtime.master.scheduler;
 
-import edu.snu.vortex.runtime.common.RuntimeAttribute;
-import edu.snu.vortex.runtime.common.comm.ExecutorMessage;
-import edu.snu.vortex.runtime.common.plan.physical.PhysicalPlan;
-import edu.snu.vortex.runtime.common.plan.physical.PhysicalStage;
 import edu.snu.vortex.runtime.common.plan.physical.TaskGroup;
-import edu.snu.vortex.runtime.common.state.JobState;
-import edu.snu.vortex.runtime.common.state.StageState;
-import edu.snu.vortex.runtime.common.state.TaskGroupState;
-import edu.snu.vortex.runtime.exception.IllegalStateTransitionException;
-import edu.snu.vortex.runtime.exception.SchedulingException;
-import edu.snu.vortex.runtime.exception.UnknownExecutionStateException;
-import edu.snu.vortex.runtime.exception.UnrecoverableFailureException;
-import edu.snu.vortex.runtime.master.ExecutionStateManager;
 import edu.snu.vortex.runtime.master.ExecutorRepresenter;
+import org.apache.reef.tang.annotations.DefaultImplementation;
 
-import java.util.*;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Optional;
+import java.util.Set;
 
 /**
- * Scheduler receives a {@link PhysicalPlan} to execute and asynchronously schedules the task groups.
- * The policy by which it schedules them is dependent on the implementation of {@link SchedulingPolicy}.
+ * Defines the policy by which {@link BatchScheduler} assigns task groups to executors.
  */
-public final class Scheduler {
-  private static final Logger LOG = Logger.getLogger(Scheduler.class.getName());
-
-  private final ExecutorService schedulerThread;
-  private final BlockingDeque<TaskGroup> taskGroupsToSchedule;
-  private final ExecutionStateManager executionStateManager;
+@DefaultImplementation(RoundRobinSchedulingPolicy.class)
+public interface SchedulingPolicy {
 
   /**
-   * A map of executor ID to the corresponding {@link ExecutorRepresenter}.
-   * This object is synchronized as multiple threads can access and modify {@link ExecutorRepresenter}s.
+   * Returns this scheduling policy's timeout before an executor assignment.
+   * @return the timeout in milliseconds.
    */
-  private final Map<String, ExecutorRepresenter> executorRepresenterMap;
+  long getScheduleTimeout();
 
   /**
-   * The {@link SchedulingPolicy} used to schedule task groups.
-   * {@link this#schedulingPolicyAttribute} decides the implementation.
+   * Attempts to schedule the given taskGroup to an executor according to this policy.
+   * If there is no executor available for the taskGroup, it waits for an executor to be assigned before it times out.
+   * (Depending on the executor's resource type)
+   *
+   * @param taskGroup to schedule
+   * @return {@link ExecutorRepresenter} on which the taskGroup is scheduled if successful, an empty Optional otherwise.
    */
-  private SchedulingPolicy schedulingPolicy;
-  private RuntimeAttribute schedulingPolicyAttribute;
+  Optional<ExecutorRepresenter> attemptSchedule(final TaskGroup taskGroup);
 
   /**
-   * Timeout for the {@link SchedulingPolicy} upon task group scheduling.
+   * Adds the executor to the pool of available executors.
+   * Unlocks this policy to schedule a next taskGroup if locked.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor that has been added.
    */
-  private long scheduleTimeout;
+  void onExecutorAdded(final ExecutorRepresenter executor);
 
   /**
-   * The list of stages for the currently running job.
-   * This object is synchronized as a new job can be submitted while a job runs.
+   * Deletes the executor from the pool of available executors.
+   * Locks this policy from scheduling if there is no more executor currently available for the next taskGroup.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor that has been deleted.
+   * @return the ids of the set of task groups that were running on the executor.
    */
-  private List<PhysicalStage> physicalStages;
-  private PhysicalStage currentStage;
-
-  public Scheduler(final ExecutionStateManager executionStateManager,
-                   final RuntimeAttribute schedulingPolicyAttribute,
-                   final long scheduleTimeout) {
-    this.executionStateManager = executionStateManager;
-
-    this.schedulerThread = Executors.newSingleThreadExecutor();
-    this.taskGroupsToSchedule = new LinkedBlockingDeque<>();
-    this.executorRepresenterMap = new HashMap<>();
-    schedulerThread.execute(new TaskGroupScheduleHandler());
-
-    // The default policy is initialized and set here.
-    this.schedulingPolicyAttribute = schedulingPolicyAttribute;
-    initializeSchedulingPolicy();
-
-    this.scheduleTimeout = scheduleTimeout;
-    this.physicalStages = Collections.emptyList();
-  }
+  Set<String> onExecutorRemoved(final ExecutorRepresenter executor);
 
   /**
-   * Receives a job to schedule.
-   * @param physicalPlan the physical plan for the job.
+   * Marks the executor scheduled for the taskGroup.
+   * Locks this policy from scheduling if there is no more executor currently available for the next taskGroup.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor assigned for the taskGroup.
+   * @param taskGroupId scheduled to the executor.
    */
-  public void scheduleJob(final PhysicalPlan physicalPlan) {
-    synchronized (physicalStages) {
-      this.physicalStages = physicalPlan.getStageDAG().getTopologicalSort();
-    }
-    initializeSchedulingPolicy();
-    executionStateManager.manageNewJob(physicalPlan);
-    scheduleNextStage();
-  }
+  void onTaskGroupScheduled(final ExecutorRepresenter executor, final String taskGroupId);
 
   /**
-   * Receives a {@link edu.snu.vortex.runtime.common.comm.ExecutorMessage.TaskGroupStateChangedMsg} from an executor.
-   * The message is received via communicator where this method is called.
-   * @param executorId the id of the executor where the message was sent from.
-   * @param message from the executor.
+   * Marks the taskGroup's completion in the executor.
+   * Unlocks this policy to schedule a next taskGroup if locked.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor where the taskGroup's execution has completed.
+   * @param taskGroupId whose execution has completed.
    */
-  // TODO #83: Introduce Task Group Executor
-  // TODO #94: Implement Distributed Communicator
-  public void onTaskGroupStateChanged(final String executorId,
-                                      final ExecutorMessage.TaskGroupStateChangedMsg message) {
-    final TaskGroupState.State newState = convertState(message.getState());
-    final boolean currentStageComplete =
-        executionStateManager.onTaskGroupStateChanged(message.getTaskGroupId(), newState);
-    switch (newState) {
-    case COMPLETE:
-      synchronized (executorRepresenterMap) {
-        onTaskGroupExecutionComplete(executorRepresenterMap.get(executorId),
-            message.getTaskGroupId(), currentStageComplete);
-      }
-      break;
-    case FAILED_RECOVERABLE:
-      synchronized (executorRepresenterMap) {
-        onTaskGroupExecutionFailed(executorRepresenterMap.get(executorId), message.getTaskGroupId(),
-            message.getFailedTaskIdsList());
-      }
-      break;
-    case FAILED_UNRECOVERABLE:
-      throw new UnrecoverableFailureException(new Exception(new StringBuffer().append("The job failed on TaskGroup #")
-          .append(message.getTaskGroupId()).append(" in Executor ").append(executorId).toString()));
-    case READY:
-    case EXECUTING:
-      throw new IllegalStateTransitionException(new Exception("The states READY/EXECUTING cannot occur at this point"));
-    default:
-      throw new UnknownExecutionStateException(new Exception("This TaskGroupState is unknown: " + newState));
-    }
-  }
-
-  private void onTaskGroupExecutionComplete(final ExecutorRepresenter executor,
-                                            final String taskGroupId,
-                                            final boolean currentStageComplete) {
-    schedulingPolicy.onTaskGroupExecutionComplete(executor, taskGroupId);
-
-    // if the current stage is complete,
-    if (currentStageComplete) {
-      final boolean jobComplete =
-          executionStateManager.onStageStateChanged(currentStage.getId(), StageState.State.COMPLETE);
-
-
-      if (jobComplete) { // and if the job is complete,
-        executionStateManager.onJobStateChanged(JobState.State.COMPLETE);
-      } else { // and if the job is still executing, schedule the next stage
-        scheduleNextStage();
-      }
-    }
-  }
-
-  // TODO #163: Handle Fault Tolerance
-  private void onTaskGroupExecutionFailed(final ExecutorRepresenter executor, final String taskGroupId,
-                                          final List<String> taskIdOnFailure) {
-    schedulingPolicy.onTaskGroupExecutionFailed(executor, taskGroupId);
-  }
-
-  // TODO #85: Introduce Resource Manager
-  public void onExecutorAdded(final ExecutorRepresenter executor) {
-    schedulingPolicy.onExecutorAdded(executor);
-    synchronized (executorRepresenterMap) {
-      executorRepresenterMap.put(executor.getExecutorId(), executor);
-    }
-  }
-
-  // TODO #163: Handle Fault Tolerance
-  // TODO #85: Introduce Resource Manager
-  public void onExecutorRemoved(final ExecutorRepresenter executor) {
-    synchronized (executorRepresenterMap) {
-      executorRepresenterMap.remove(executor.getExecutorId());
-    }
-    final Set<String> taskGroupsToReschedule = schedulingPolicy.onExecutorRemoved(executor);
-
-    // Reschedule taskGroupsToReschedule
-  }
+  void onTaskGroupExecutionComplete(final ExecutorRepresenter executor, final String taskGroupId);
 
   /**
-   * Schedules the next stage to execute.
-   * It adds the list of task groups for the stage where the scheduler thread continuously polls from.
+   * Marks the taskGroup's failure in the executor.
+   * Unlocks this policy to reschedule this taskGroup if locked.
+   * (Depending on the executor's resource type)
+   *
+   * @param executor where the taskGroup's execution has failed.
+   * @param taskGroupId whose execution has completed.
    */
-  private synchronized void scheduleNextStage() {
-    synchronized (physicalStages) {
-      currentStage = physicalStages.remove(0);
-    }
-    taskGroupsToSchedule.addAll(currentStage.getTaskGroupList());
-    executionStateManager.onStageStateChanged(currentStage.getId(), StageState.State.EXECUTING);
-  }
-
-  /**
-   * Initializes the scheduling policy.
-   * This can be called anytime during this scheduler's lifetime and the policy will change flexibly.
-   */
-  private void initializeSchedulingPolicy() {
-    switch (schedulingPolicyAttribute) {
-    case Batch:
-      this.schedulingPolicy = new RoundRobinSchedulingPolicy(scheduleTimeout);
-      break;
-    default:
-      throw new SchedulingException(new Exception("The scheduling policy is unsupported by runtime"));
-    }
-  }
-
-  /**
-   * A separate thread is run to schedule task groups to executors.
-   */
-  private class TaskGroupScheduleHandler implements Runnable {
-    @Override
-    public void run() {
-      while (!schedulerThread.isShutdown()) {
-        try {
-          final TaskGroup taskGroup = taskGroupsToSchedule.takeFirst();
-          final Optional<ExecutorRepresenter> executor = schedulingPolicy.attemptSchedule(taskGroup);
-          System.out.println(taskGroup);
-          System.out.println(executor);
-          if (!executor.isPresent()) {
-            LOG.log(Level.INFO, "Failed to assign an executor before the timeout: {0}",
-                schedulingPolicy.getScheduleTimeout());
-            taskGroupsToSchedule.addLast(taskGroup);
-          } else {
-            // TODO #83: Introduce Task Group Executor
-            // TODO #94: Implement Distributed Communicator
-            // Must send this taskGroup to the destination executor.
-            schedulingPolicy.onTaskGroupScheduled(executor.get(), taskGroup.getTaskGroupId());
-            System.out.println("Scheduling! " + taskGroup.getTaskGroupId());
-            executionStateManager.onTaskGroupStateChanged(taskGroup.getTaskGroupId(),
-                TaskGroupState.State.EXECUTING);
-          }
-        } catch (final Exception e) {
-          throw new SchedulingException(e);
-        }
-      }
-    }
-  }
-
-  public void terminate() {
-    schedulerThread.shutdown();
-    taskGroupsToSchedule.clear();
-  }
-
-  // TODO #164: Cleanup Protobuf Usage
-  private TaskGroupState.State convertState(final ExecutorMessage.TaskGroupStateFromExecutor state) {
-    switch (state) {
-    case READY:
-      return TaskGroupState.State.READY;
-    case EXECUTING:
-      return TaskGroupState.State.EXECUTING;
-    case COMPLETE:
-      return TaskGroupState.State.COMPLETE;
-    case FAILED_RECOVERABLE:
-      return TaskGroupState.State.FAILED_RECOVERABLE;
-    case FAILED_UNRECOVERABLE:
-      return TaskGroupState.State.FAILED_UNRECOVERABLE;
-    default:
-      throw new UnknownExecutionStateException(new Exception("This TaskGroupState is unknown: " + state));
-    }
-  }
+  void onTaskGroupExecutionFailed(final ExecutorRepresenter executor, final String taskGroupId);
 }
