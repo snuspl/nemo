f1d1cc69033a4dea2a4c75b0c561560230732a40
Joo Yeon Kim <jykim88@gmail.com>
2017-04-30 15:44:48 +0900

    Add InputReader/OutputWriter

--- src/main/java/edu/snu/vortex/runtime/executor/datatransfer/InputReader.java
+++ src/main/java/edu/snu/vortex/runtime/executor/datatransfer/InputReader.java
@@ -13,22 +13,77 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.snu.vortex.runtime.executor.channel;
+package edu.snu.vortex.runtime.executor.datatransfer;
 
 import edu.snu.vortex.compiler.ir.Element;
+import edu.snu.vortex.runtime.common.RuntimeAttribute;
+import edu.snu.vortex.runtime.common.plan.RuntimeEdge;
+import edu.snu.vortex.runtime.common.plan.logical.RuntimeVertex;
+import edu.snu.vortex.runtime.common.plan.physical.Task;
+import edu.snu.vortex.runtime.exception.UnsupportedCommPatternException;
+import edu.snu.vortex.runtime.executor.dataplacement.DataPlacement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Input channel interface.
  */
 public final class InputReader extends DataTransfer {
 
-  public InputReader(final String id) {
-    super(id);
+  private final Task dstTask;
+  private final RuntimeVertex srcRuntimeVertex;
+  private final RuntimeEdge runtimeEdge;
+  private final DataPlacement dataPlacement;
+
+  public InputReader(final Task dstTask,
+                     final RuntimeVertex srcRuntimeVertex,
+                     final RuntimeEdge runtimeEdge,
+                     final DataPlacement dataPlacement) {
+    super(runtimeEdge.getRuntimeEdgeId());
+    this.dstTask = dstTask;
+    this.srcRuntimeVertex = srcRuntimeVertex;
+    this.runtimeEdge = runtimeEdge;
+    this.dataPlacement = dataPlacement;
   }
 
   public Iterable<Element> read() {
-    final Iterable<Element> data = null;
+    switch (srcRuntimeVertex.getVertexAttributes().get(RuntimeAttribute.Key.CommPattern)) {
+    case OneToOne:
+      return readOneToOne();
+    case Broadcast:
+      return readBroadcast();
+    case ScatterGather:
+      return readScatterGather();
+    default:
+      throw new UnsupportedCommPatternException(new Exception("Communication pattern not supported"));
+    }
+  }
 
-    return data;
+  public Iterable<Element> readOneToOne() {
+    return dataPlacement.get(runtimeEdge.getRuntimeEdgeId(), dstTask.getIndex());
+  }
+
+  public Iterable<Element> readBroadcast() {
+    final int numSrcTasks = srcRuntimeVertex.getVertexAttributes().get(RuntimeAttribute.IntegerKey.Parallelism);
+
+    final List<Element> readData = new ArrayList<>();
+    for (int srcTaskIdx = 0; srcTaskIdx < numSrcTasks; srcTaskIdx++) {
+      final Iterable<Element> dataFromATask = dataPlacement.get(runtimeEdge.getRuntimeEdgeId(), srcTaskIdx);
+      dataFromATask.forEach(element -> readData.add(element));
+    }
+    return readData;
+  }
+
+  public Iterable<Element> readScatterGather() {
+    final int numSrcTasks = srcRuntimeVertex.getVertexAttributes().get(RuntimeAttribute.IntegerKey.Parallelism);
+
+    final List<Element> readData = new ArrayList<>();
+    for (int srcTaskIdx = 0; srcTaskIdx < numSrcTasks; srcTaskIdx++) {
+      final Iterable<Element> dataFromATask =
+          dataPlacement.get(runtimeEdge.getRuntimeEdgeId(), srcTaskIdx, dstTask.getIndex());
+      dataFromATask.forEach(element -> readData.add(element));
+    }
+    return readData;
   }
 }
--- src/main/java/edu/snu/vortex/runtime/executor/datatransfer/OutputWriter.java
+++ src/main/java/edu/snu/vortex/runtime/executor/datatransfer/OutputWriter.java
@@ -13,24 +13,93 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.snu.vortex.runtime.executor.channel;
+package edu.snu.vortex.runtime.executor.datatransfer;
 
 import edu.snu.vortex.compiler.ir.Element;
+import edu.snu.vortex.runtime.common.RuntimeAttribute;
+import edu.snu.vortex.runtime.common.plan.RuntimeEdge;
+import edu.snu.vortex.runtime.common.plan.logical.RuntimeVertex;
+import edu.snu.vortex.runtime.common.plan.physical.Task;
+import edu.snu.vortex.runtime.exception.UnsupportedCommPatternException;
+import edu.snu.vortex.runtime.exception.UnsupportedPartitionerException;
+import edu.snu.vortex.runtime.executor.dataplacement.DataPlacement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Output channel interface.
  */
 public final class OutputWriter extends DataTransfer {
+  private final Task srcTask;
+  private final RuntimeVertex dstRuntimeVertex;
+  private final RuntimeEdge runtimeEdge;
+  private final DataPlacement dataPlacement;
 
-  public OutputWriter(final String id) {
-    super(id);
+  public OutputWriter(final Task srcTask,
+                      final RuntimeVertex dstRuntimeVertex,
+                      final RuntimeEdge runtimeEdge,
+                      final DataPlacement dataPlacement) {
+    super(runtimeEdge.getRuntimeEdgeId());
+    this.srcTask = srcTask;
+    this.dstRuntimeVertex = dstRuntimeVertex;
+    this.runtimeEdge = runtimeEdge;
+    this.dataPlacement = dataPlacement;
   }
 
   /**
-   * write data to the channel.
-   * @param data An iterable for elements to be written.
+   * write dataToWrite to the channel.
+   * @param dataToWrite An iterable for elements to be written.
    */
-  public void write(final Iterable<Element> data) {
+  public void write(final Iterable<Element> dataToWrite) {
+    switch (dstRuntimeVertex.getVertexAttributes().get(RuntimeAttribute.Key.CommPattern)) {
+    case OneToOne:
+      writeOneToOne(dataToWrite);
+      break;
+    case Broadcast:
+      writeBroadcast(dataToWrite);
+      break;
+    case ScatterGather:
+      writeScatterGather(dataToWrite);
+      break;
+    default:
+      throw new UnsupportedCommPatternException(new Exception("Communication pattern not supported"));
+    }
+  }
 
+  private void writeOneToOne(final Iterable<Element> dataToWrite) {
+    dataPlacement.put(runtimeEdge.getRuntimeEdgeId(), srcTask.getIndex(), dataToWrite);
+  }
+
+  private void writeBroadcast(final Iterable<Element> dataToWrite) {
+    dataPlacement.put(runtimeEdge.getRuntimeEdgeId(), srcTask.getIndex(), dataToWrite);
+  }
+
+  private void writeScatterGather(final Iterable<Element> dataToWrite) {
+    final RuntimeAttribute partitioner = runtimeEdge.getEdgeAttributes().get(RuntimeAttribute.Key.Partition);
+    final int dstParallelism = dstRuntimeVertex.getVertexAttributes().get(RuntimeAttribute.IntegerKey.Parallelism);
+
+    switch (partitioner) {
+    case Hash:
+      // First partition the data to write,
+      final List<List<Element>> partitionedOutputList = new ArrayList<>(dstParallelism);
+      IntStream.range(0, dstParallelism).forEach(partitionIdx -> partitionedOutputList.add(new ArrayList<>()));
+      dataToWrite.forEach(element -> {
+        // Hash the data by its key, and "modulo" the number of destination tasks.
+        final int dstIdx = Math.abs(element.getKey().hashCode() % dstParallelism);
+        partitionedOutputList.get(dstIdx).add(element);
+      });
+
+      // Then write each partition appropriately to the target data placement.
+      IntStream.range(0, dstParallelism).forEach(partitionIdx ->
+        dataPlacement.put(
+            runtimeEdge.getRuntimeEdgeId(), srcTask.getIndex(), partitionIdx, partitionedOutputList.get(partitionIdx)));
+      break;
+    case Range:
+    default:
+      throw new UnsupportedPartitionerException(
+          new Exception(partitioner + " partitioning not yet supported"));
+    }
   }
 }
