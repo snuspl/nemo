42dd7f9bfec9d172086b7a51752ccdfbac5502f7
Joo Yeon Kim <jykim88@gmail.com>
2017-05-09 11:20:37 +0900

    Instantiate executors in LocalResourceManager

--- src/main/java/edu/snu/vortex/runtime/master/resourcemanager/LocalResourceManager.java
+++ src/main/java/edu/snu/vortex/runtime/master/resourcemanager/LocalResourceManager.java
@@ -4,9 +4,13 @@ import edu.snu.vortex.runtime.common.RuntimeAttribute;
 import edu.snu.vortex.runtime.common.RuntimeIdGenerator;
 import edu.snu.vortex.runtime.common.message.MessageEnvironment;
 import edu.snu.vortex.runtime.common.message.MessageSender;
+import edu.snu.vortex.runtime.executor.Executor;
+import edu.snu.vortex.runtime.master.BlockManagerMaster;
 import edu.snu.vortex.runtime.master.scheduler.Scheduler;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@inheritDoc}
@@ -15,12 +19,17 @@ import java.io.Serializable;
 public final class LocalResourceManager implements ResourceManager {
 
   private final Scheduler scheduler;
+  private final Map<String, Executor> executorMap;
   private final MessageEnvironment messageEnvironment;
+  private final BlockManagerMaster blockManagerMaster;
 
   public LocalResourceManager(final Scheduler scheduler,
-                              final MessageEnvironment messageEnvironment) {
+                              final MessageEnvironment messageEnvironment,
+                              final BlockManagerMaster blockManagerMaster) {
     this.scheduler = scheduler;
     this.messageEnvironment = messageEnvironment;
+    this.blockManagerMaster = blockManagerMaster;
+    this.executorMap = new HashMap<>();
   }
 
   @Override
@@ -36,10 +45,8 @@ public final class LocalResourceManager implements ResourceManager {
 
     final ExecutorRepresenter executorRepresenter =
         new ExecutorRepresenter(executorId, resourceType, executorCapacity, messageSender);
-
-    // TODO #83: Introduce Task Group Executor
-    // Instantiate executors.
-
+    final Executor executor = new Executor(executorId, executorCapacity, messageEnvironment, blockManagerMaster);
+    executorMap.put(executorId, executor);
     scheduler.onExecutorAdded(executorRepresenter);
   }
 }
