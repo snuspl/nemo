ac71420be26c6385b88306b254f054f19dca944a
Joo Yeon Kim <jykim88@gmail.com>
2017-05-10 21:08:23 +0900

    Exception added

---
+++ src/main/java/edu/snu/vortex/runtime/exception/IllegalMessageException.java
@@ -0,0 +1,15 @@
+package edu.snu.vortex.runtime.exception;
+
+/**
+ * IllegalMessageException.
+ * Thrown when the received message is of an illegal type in master/executor.
+ */
+public final class IllegalMessageException extends RuntimeException {
+  /**
+   * IllegalMessageException.
+   * @param cause cause
+   */
+  public IllegalMessageException(final Throwable cause) {
+    super(cause);
+  }
+}
