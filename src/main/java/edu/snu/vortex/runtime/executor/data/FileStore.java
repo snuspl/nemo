0f6d1bc6e3e2bada1dfe808134091868bc9018a5
sanha <sanhaleehana@naver.com>
2017-07-10 16:24:14 +0900

    Simple divide

--- src/main/java/edu/snu/vortex/runtime/executor/data/FileStore.java
+++ src/main/java/edu/snu/vortex/runtime/executor/data/FileStore.java
@@ -37,13 +37,16 @@ import java.util.concurrent.ConcurrentHashMap;
 final class FileStore implements PartitionStore {
 
   private final String fileDirectory;
+  private final int blockSize;
   private final Map<String, FilePartition> partitionIdToData;
   private final InjectionFuture<PartitionManagerWorker> partitionManagerWorker;
 
   @Inject
   private FileStore(@Parameter(JobConf.FileDirectory.class) final String fileDirectory,
+                    @Parameter(JobConf.BlockSize.class) final int blockSize,
                     final InjectionFuture<PartitionManagerWorker> partitionManagerWorker) {
     this.fileDirectory = fileDirectory;
+    this.blockSize = blockSize * 1000;
     this.partitionIdToData = new ConcurrentHashMap<>();
     this.partitionManagerWorker = partitionManagerWorker;
     new File(fileDirectory).mkdirs();
@@ -75,34 +78,55 @@ final class FileStore implements PartitionStore {
    */
   @Override
   public Optional<Long> putPartition(final String partitionId, final Iterable<Element> data) {
-    final FilePartition partition = new FilePartition();
+    final PartitionManagerWorker worker = partitionManagerWorker.get();
+    final String runtimeEdgeId = partitionId.split("_")[1];
+    final Coder coder = worker.getCoder(runtimeEdgeId);
+    final FilePartition partition = new FilePartition(coder, fileDirectory + "/" + partitionId);
     final Partition previousPartition = partitionIdToData.putIfAbsent(partitionId, partition);
     if (previousPartition != null) {
       throw new RuntimeException("Trying to overwrite an existing partition");
     }
 
-    // Serialize the given data
-    final PartitionManagerWorker worker = partitionManagerWorker.get();
-    final String runtimeEdgeId = partitionId.split("_")[1];
-    final Coder coder = worker.getCoder(runtimeEdgeId);
-    final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    long length = 0;
-    // TODO 301: Divide a Task's Output Partitions into Smaller Blocks.
+    // Serialize the given data into blocks
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream(blockSize);
+    long elementsInBlock = 0;
+    long partitionSize = 0;
     for (final Element element : data) {
       coder.encode(element, outputStream);
-      length++;
-    }
-    try {
-      outputStream.close();
-    } catch (final IOException e) {
-      throw new RuntimeException(e);
-    }
-    final byte[] serialized = outputStream.toByteArray();
+      elementsInBlock++;
 
-    // Synchronously write the serialized data to file
-    partition.writeData(serialized, coder, fileDirectory + "/" + partitionId, length);
+      if (outputStream.size() >= blockSize) {
+        // If this block is large enough, synchronously append it to the file and create a new buffer
+        try {
+          outputStream.close();
+        } catch (final IOException e) {
+          throw new RuntimeException(e);
+        }
 
-    return Optional.of(Long.valueOf(serialized.length));
+        final byte[] serialized = outputStream.toByteArray();
+        partitionSize += serialized.length;
+        partition.writeBlock(serialized, elementsInBlock);
+
+        outputStream = new ByteArrayOutputStream(blockSize);
+        elementsInBlock = 0;
+      }
+    }
+
+    if (outputStream.size() > 0) {
+      // If there are any remaining data in stream, write it as another block.
+      try {
+        outputStream.close();
+      } catch (final IOException e) {
+        throw new RuntimeException(e);
+      }
+
+      final byte[] serialized = outputStream.toByteArray();
+      partitionSize += serialized.length;
+      partition.writeBlock(serialized, elementsInBlock);
+    }
+    partition.finishWrite();
+
+    return Optional.of(partitionSize);
   }
 
   /**
