e1890dd87e60578df1b208c4084ad712b415c28d
sanha <sanhaleehana@naver.com>
2017-07-13 14:23:37 +0900

    blocking queue

--- src/main/java/edu/snu/vortex/runtime/executor/TaskGroupExecutor.java
+++ src/main/java/edu/snu/vortex/runtime/executor/TaskGroupExecutor.java
@@ -15,6 +15,7 @@
  */
 package edu.snu.vortex.runtime.executor;
 
+import edu.snu.vortex.common.Pair;
 import edu.snu.vortex.compiler.ir.Element;
 import edu.snu.vortex.compiler.ir.Reader;
 import edu.snu.vortex.compiler.ir.Transform;
@@ -36,11 +37,13 @@ import edu.snu.vortex.runtime.master.irimpl.OutputCollectorImpl;
 import edu.snu.vortex.common.dag.DAG;
 
 import java.util.*;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Executes a task group.
@@ -252,21 +255,29 @@ public final class TaskGroupExecutor {
     transform.prepare(transformContext, outputCollector);
 
     // Check for non-side inputs
+    // This blocking queue contains the pairs having data and source vertex ids.
+    final BlockingQueue<Pair<Iterable<Element>, String>> dataQueue = new LinkedBlockingQueue<>();
     taskIdToInputReaderMap.get(operatorTask.getId())
         .stream()
         .filter(inputReader -> !inputReader.isSideInputReader())
         .forEach(inputReader -> {
-          // TODO #335: Introduce producer - consumer structure to task group execution
           final List<CompletableFuture<Iterable<Element>>> futures = inputReader.read();
-          final Iterable<Element> data;
-          try {
-             data = InputReader.combineFutures(futures);
-          } catch (InterruptedException | ExecutionException e) {
-            throw new PartitionFetchException(e);
-          }
-
-          transform.onData(data, inputReader.getSrcRuntimeVertexId());
+          final String srcVtxId = inputReader.getSrcRuntimeVertexId();
+          // Add consumers which will push the data to the data queue when it ready to the futures.
+          futures.forEach(compFuture -> compFuture.thenAccept(data -> dataQueue.add(Pair.of(data, srcVtxId))));
         });
+
+    // Consumes all of the partitions from incoming edges.
+    final int numSrcTasks = taskGroup.getTaskDAG().getIncomingEdgesOf(operatorTask).size();
+    IntStream.range(0, numSrcTasks).forEach(srcTaskNum -> {
+      try {
+        // Because the data queue is a blocking queue, we may need to wait some available data to be pushed.
+        final Pair<Iterable<Element>, String> availableData = dataQueue.take();
+        transform.onData(availableData.left(), availableData.right());
+      } catch (final InterruptedException e) {
+        throw new PartitionFetchException(e);
+      }
+    });
     transform.close();
 
     final Iterable<Element> output = outputCollector.getOutputList();
