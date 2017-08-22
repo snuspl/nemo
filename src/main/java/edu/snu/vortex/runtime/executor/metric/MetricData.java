f957d432474d898adfbb800fb73778bec3669ac0
Jeongyoon Eo <jeongyoon0807@gmail.com>
2017-08-23 01:43:35 +0900

    Minor cleanup

--- src/main/java/edu/snu/vortex/runtime/executor/metric/MetricData.java
+++ src/main/java/edu/snu/vortex/runtime/executor/metric/MetricData.java
@@ -16,9 +16,6 @@
 package edu.snu.vortex.runtime.executor.metric;
 
 import java.util.*;
-//import com.fasterxml.jackson.core.type.TypeReference;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 /**
