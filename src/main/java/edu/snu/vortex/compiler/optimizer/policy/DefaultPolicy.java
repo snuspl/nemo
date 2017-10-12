84e5b4546a69de2751f3ed7b6f5b00adc9ed4267
Won Wook SONG <wsong0512@gmail.com>
2017-10-12 17:49:11 +0900

    fix to make master run

--- src/main/java/edu/snu/vortex/compiler/optimizer/policy/DefaultPolicy.java
+++ src/main/java/edu/snu/vortex/compiler/optimizer/policy/DefaultPolicy.java
@@ -15,7 +15,6 @@
  */
 package edu.snu.vortex.compiler.optimizer.policy;
 
-import edu.snu.vortex.compiler.optimizer.pass.compiletime.annotating.DefaultPartitionerPass;
 import edu.snu.vortex.compiler.optimizer.pass.compiletime.annotating.DefaultStagePartitioningPass;
 import edu.snu.vortex.compiler.optimizer.pass.compiletime.annotating.ScheduleGroupPass;
 import edu.snu.vortex.compiler.optimizer.pass.compiletime.CompileTimePass;
@@ -39,20 +38,11 @@ public final class DefaultPolicy implements Policy {
 
   @Override
   public List<CompileTimePass> getCompileTimePasses() {
-<<<<<<< HEAD
     return this.policyBuilder.build().getCompileTimePasses();
   }
 
   @Override
   public List<RuntimePass<?>> getRuntimePasses() {
     return this.policyBuilder.build().getRuntimePasses();
-=======
-    return Arrays.asList(
-        new ParallelismPass(), // Provides parallelism information.
-        new DefaultPartitionerPass(), // TODO #515: Move to InitializePass
-        new DefaultStagePartitioningPass(),
-        new ScheduleGroupPass()
-    );
->>>>>>> master
   }
 }
