da5efcc14331207db329caa888e1f643a1565e4a
Won Wook SONG <wsong0512@gmail.com>
2017-11-07 13:06:02 +0900

    address comments

--- src/test/java/edu/snu/onyx/compiler/optimizer/policy/PolicyBuilderTest.java
+++ src/test/java/edu/snu/onyx/compiler/optimizer/policy/PolicyBuilderTest.java
@@ -4,7 +4,6 @@ import edu.snu.onyx.compiler.exception.CompileTimeOptimizationException;
 import edu.snu.onyx.compiler.optimizer.pass.compiletime.annotating.DefaultStagePartitioningPass;
 import edu.snu.onyx.compiler.optimizer.pass.compiletime.annotating.ScheduleGroupPass;
 import edu.snu.onyx.compiler.optimizer.pass.compiletime.composite.*;
-import edu.snu.onyx.compiler.optimizer.pass.runtime.DataSkewRuntimePass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,39 +12,21 @@ import static org.junit.Assert.assertTrue;
 public final class PolicyBuilderTest {
   @Test
   public void testDisaggregationPolicy() {
-    final Policy disaggregationPolicy = new PolicyBuilder()
-        .registerCompileTimePass(new InitiationCompositePass())
-        .registerCompileTimePass(new LoopOptimizationCompositePass())
-        .registerCompileTimePass(new DisaggregationPass())
-        .registerCompileTimePass(new DefaultStagePartitioningPass())
-        .registerCompileTimePass(new ScheduleGroupPass())
-        .build();
+    final Policy disaggregationPolicy = new DisaggregationPolicy();
     assertEquals(33, disaggregationPolicy.getCompileTimePasses().size());
     assertEquals(0, disaggregationPolicy.getRuntimePasses().size());
   }
 
   @Test
   public void testPadoPolicy() {
-    final Policy padoPolicy = new PolicyBuilder()
-        .registerCompileTimePass(new InitiationCompositePass())
-        .registerCompileTimePass(new LoopOptimizationCompositePass())
-        .registerCompileTimePass(new PadoCompositePass())
-        .registerCompileTimePass(new DefaultStagePartitioningPass())
-        .registerCompileTimePass(new ScheduleGroupPass())
-        .build();
+    final Policy padoPolicy = new PadoPolicy();
     assertEquals(34, padoPolicy.getCompileTimePasses().size());
     assertEquals(0, padoPolicy.getRuntimePasses().size());
   }
 
   @Test
   public void testDataSkewPolicy() {
-    final Policy dataSkewPolicy = new PolicyBuilder()
-        .registerCompileTimePass(new InitiationCompositePass())
-        .registerCompileTimePass(new LoopOptimizationCompositePass())
-        .registerRuntimePass(new DataSkewRuntimePass(), new DataSkewCompositePass())
-        .registerCompileTimePass(new DefaultStagePartitioningPass())
-        .registerCompileTimePass(new ScheduleGroupPass())
-        .build();
+    final Policy dataSkewPolicy = new DataSkewPolicy();
     assertEquals(41, dataSkewPolicy.getCompileTimePasses().size());
     assertEquals(1, dataSkewPolicy.getRuntimePasses().size());
   }
@@ -58,7 +39,7 @@ public final class PolicyBuilderTest {
           .registerCompileTimePass(new DefaultStagePartitioningPass())
           .registerCompileTimePass(new ScheduleGroupPass())
           .build();
-    } catch (Exception e) {
+    } catch (Exception e) { // throw an exception if default execution properties are not set.
       assertTrue(e instanceof CompileTimeOptimizationException);
       assertTrue(e.getMessage().contains("Prerequisite ExecutionProperty hasn't been met"));
     }
