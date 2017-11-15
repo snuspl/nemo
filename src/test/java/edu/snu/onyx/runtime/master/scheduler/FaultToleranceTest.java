59da7c1eb8f515b48553ebfc1729dd37674488f4
Joo Yeon Kim <jykim88@gmail.com>
2017-11-15 16:46:47 +0900

    Comments and fix bug

--- src/test/java/edu/snu/onyx/runtime/master/scheduler/FaultToleranceTest.java
+++ src/test/java/edu/snu/onyx/runtime/master/scheduler/FaultToleranceTest.java
@@ -59,6 +59,7 @@ import java.util.*;
 
 import static edu.snu.onyx.runtime.common.state.StageState.State.EXECUTING;
 import static edu.snu.onyx.runtime.common.state.StageState.State.READY;
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -172,11 +173,11 @@ public final class FaultToleranceTest {
     final IREdge e2 = new IREdge(ScatterGather.class, v3, v2, Coder.DUMMY_CODER);
     irDAGBuilder.connectVertices(e2);
 
-    final IREdge e4 = new IREdge(ScatterGather.class, v2, v4, Coder.DUMMY_CODER);
-    irDAGBuilder.connectVertices(e4);
+    final IREdge e3 = new IREdge(ScatterGather.class, v2, v4, Coder.DUMMY_CODER);
+    irDAGBuilder.connectVertices(e3);
 
-    final IREdge e5 = new IREdge(OneToOne.class, v4, v5, Coder.DUMMY_CODER);
-    irDAGBuilder.connectVertices(e5);
+    final IREdge e4 = new IREdge(OneToOne.class, v4, v5, Coder.DUMMY_CODER);
+    irDAGBuilder.connectVertices(e4);
 
     final DAG<IRVertex, IREdge> irDAG = Optimizer.optimize(irDAGBuilder.buildWithoutSourceSinkCheck(),
         new TestPolicy(), "");
@@ -206,6 +207,7 @@ public final class FaultToleranceTest {
         stage.getTaskGroupList().forEach(taskGroup ->
             jobStateManager.onTaskGroupStateChanged(taskGroup, TaskGroupState.State.EXECUTING));
 
+        // Due to round robin scheduling, "a2" is assured to have a running TaskGroup.
         scheduler.onExecutorRemoved("a2");
 
         while (jobStateManager.getStageState(stage.getId()).getStateMachine().getCurrentState() != EXECUTING) {
@@ -225,7 +227,7 @@ public final class FaultToleranceTest {
               taskGroup.getTaskGroupId(), TaskGroupState.State.COMPLETE, 1, null);
         });
       } else {
-        // There are 3 executors, each of capacity 2, and there are 2 TaskGroups in ScheduleGroup 2.
+        // There are 2 executors, each of capacity 2, and there are 2 TaskGroups in ScheduleGroup 2.
         // Schedule only the first TaskGroup
         RuntimeTestUtil.mockSchedulerRunner(pendingTaskGroupQueue, schedulingPolicy, true);
 
@@ -234,6 +236,8 @@ public final class FaultToleranceTest {
           // When a TaskGroup fails while the siblings are still in the queue,
           if (isFirstTaskGroup) {
             jobStateManager.onTaskGroupStateChanged(taskGroup, TaskGroupState.State.EXECUTING);
+
+            // Due to round robin scheduling, "a3" is assured to have a running TaskGroup.
             scheduler.onExecutorRemoved("a3");
             isFirstTaskGroup = false;
           } else {
@@ -243,6 +247,98 @@ public final class FaultToleranceTest {
                 TaskGroupState.State.READY);
           }
         }
+      }
+    }
+
+    RuntimeTestUtil.cleanup();
+  }
+
+  /**
+   * Tests fault tolerance after an output write failure.
+   */
+  @Test(timeout=10000)
+  public void testOutputFailure() throws Exception {
+    // Build DAG
+    final Transform t = new EmptyComponents.EmptyTransform("empty");
+    final IRVertex v1 = new OperatorVertex(t);
+    v1.setProperty(ParallelismProperty.of(3));
+    v1.setProperty(ExecutorPlacementProperty.of(ExecutorPlacementProperty.COMPUTE));
+    irDAGBuilder.addVertex(v1);
+
+    final IRVertex v2 = new OperatorVertex(t);
+    v2.setProperty(ParallelismProperty.of(2));
+    v2.setProperty(ExecutorPlacementProperty.of(ExecutorPlacementProperty.COMPUTE));
+    irDAGBuilder.addVertex(v2);
+
+    final IRVertex v3 = new OperatorVertex(t);
+    v3.setProperty(ParallelismProperty.of(3));
+    v3.setProperty(ExecutorPlacementProperty.of(ExecutorPlacementProperty.COMPUTE));
+    irDAGBuilder.addVertex(v3);
+
+    final IRVertex v4 = new OperatorVertex(t);
+    v4.setProperty(ParallelismProperty.of(2));
+    v4.setProperty(ExecutorPlacementProperty.of(ExecutorPlacementProperty.COMPUTE));
+    irDAGBuilder.addVertex(v4);
+
+    final IRVertex v5 = new OperatorVertex(new DoTransform(null, null));
+    v5.setProperty(ParallelismProperty.of(2));
+    v5.setProperty(ExecutorPlacementProperty.of(ExecutorPlacementProperty.COMPUTE));
+    irDAGBuilder.addVertex(v5);
+
+    final IREdge e1 = new IREdge(ScatterGather.class, v1, v2, Coder.DUMMY_CODER);
+    irDAGBuilder.connectVertices(e1);
+
+    final IREdge e2 = new IREdge(ScatterGather.class, v3, v2, Coder.DUMMY_CODER);
+    irDAGBuilder.connectVertices(e2);
+
+    final IREdge e3 = new IREdge(ScatterGather.class, v2, v4, Coder.DUMMY_CODER);
+    irDAGBuilder.connectVertices(e3);
+
+    final IREdge e4 = new IREdge(OneToOne.class, v4, v5, Coder.DUMMY_CODER);
+    irDAGBuilder.connectVertices(e4);
+
+    final DAG<IRVertex, IREdge> irDAG = Optimizer.optimize(irDAGBuilder.buildWithoutSourceSinkCheck(),
+        new TestPolicy(), "");
+    final DAG<PhysicalStage, PhysicalStageEdge> physicalDAG = irDAG.convert(physicalPlanGenerator);
+
+    final PhysicalPlan plan = new PhysicalPlan("TestPlan", physicalDAG, physicalPlanGenerator.getTaskIRVertexMap());
+    final JobStateManager jobStateManager =
+        new JobStateManager(plan, partitionManagerMaster, metricMessageHandler, MAX_SCHEDULE_ATTEMPT);
+    scheduler.scheduleJob(plan, jobStateManager);
+
+    final List<PhysicalStage> dagOf4Stages = plan.getStageDAG().getTopologicalSort();
+
+    for (final PhysicalStage stage : dagOf4Stages) {
+      if (stage.getScheduleGroupIndex() == 0) {
+
+        // There are 3 executors, each of capacity 2, and there are 6 TaskGroups in ScheduleGroup 0.
+        RuntimeTestUtil.mockSchedulerRunner(pendingTaskGroupQueue, schedulingPolicy, false);
+        assertTrue(pendingTaskGroupQueue.isEmpty());
+        stage.getTaskGroupList().forEach(taskGroup -> {
+          jobStateManager.onTaskGroupStateChanged(taskGroup, TaskGroupState.State.EXECUTING);
+          RuntimeTestUtil.sendTaskGroupStateEventToScheduler(scheduler, containerManager,
+              taskGroup.getTaskGroupId(), TaskGroupState.State.COMPLETE, 1, null);
+        });
+      } else if (stage.getScheduleGroupIndex() == 1) {
+        // There are 3 executors, each of capacity 2, and there are 2 TaskGroups in ScheduleGroup 1.
+        RuntimeTestUtil.mockSchedulerRunner(pendingTaskGroupQueue, schedulingPolicy, false);
+        assertTrue(pendingTaskGroupQueue.isEmpty());
+        stage.getTaskGroupList().forEach(taskGroup -> {
+          jobStateManager.onTaskGroupStateChanged(taskGroup, TaskGroupState.State.EXECUTING);
+          RuntimeTestUtil.sendTaskGroupStateEventToScheduler(scheduler, containerManager,
+              taskGroup.getTaskGroupId(), TaskGroupState.State.FAILED_RECOVERABLE, 1,
+              TaskGroupState.RecoverableFailureCause.OUTPUT_WRITE_FAILURE);
+        });
+
+        while (jobStateManager.getStageState(stage.getId()).getStateMachine().getCurrentState() != EXECUTING) {
+
+        }
+
+        assertEquals(jobStateManager.getAttemptCountForStage(stage.getId()), 3);
+        assertFalse(pendingTaskGroupQueue.isEmpty());
+        stage.getTaskGroupList().forEach(taskGroup ->
+            assertEquals(jobStateManager.getTaskGroupState(taskGroup.getTaskGroupId()).getStateMachine().getCurrentState(),
+                TaskGroupState.State.READY));
       }
     }
 
