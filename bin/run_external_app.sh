58b101d46cedfa966fe8925e02adbdbaf7383d1d
Jeongyoon Eo <jeongyoon0807@gmail.com>
2017-11-28 22:37:10 +0900

    readme

--- bin/run_external_app.sh
+++ bin/run_external_app.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path
-java -cp ../target/onyx-0.1-SNAPSHOT-shaded.jar:$1:`yarn classpath` edu.snu.onyx.client.JobLauncher "${@:2}"
+java -cp tests/target/onyx-tests-0.1-SNAPSHOT.jar:$1:`yarn classpath` edu.snu.onyx.client.JobLauncher "${@:2}"
