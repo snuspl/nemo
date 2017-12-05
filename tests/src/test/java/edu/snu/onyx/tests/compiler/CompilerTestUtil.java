2ad9f7ac2924a838d47bbe81a7b556667c903eb2
Jeongyoon Eo <jeongyoon0807@gmail.com>
2017-12-05 15:01:24 +0900

    mysterious plan-logical

--- tests/src/test/java/edu/snu/onyx/tests/compiler/CompilerTestUtil.java
+++ tests/src/test/java/edu/snu/onyx/tests/compiler/CompilerTestUtil.java
@@ -86,13 +86,11 @@ public final class CompilerTestUtil {
     final String input = rootDir + "/../examples/src/main/resources/sample_input_als";
     final String numFeatures = "10";
     final String numIteration = "3";
-    final String dagDirectory = CompilerTestUtil.rootDir + "/dag";
 
     final ArgBuilder alsArgBuilder = new ArgBuilder()
         .addJobId(AlternatingLeastSquareInefficient.class.getSimpleName())
         .addUserMain(alsInefficient)
         .addUserArgs(input, numFeatures, numIteration)
-        .addDAGDirectory(dagDirectory);
     return compileDAG(alsArgBuilder.build());
   }
 
