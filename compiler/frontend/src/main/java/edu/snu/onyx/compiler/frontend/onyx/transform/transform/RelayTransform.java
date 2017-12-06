8e7ea8542c1644d1f7eb81daaa0b625745f6b6f6
sanha <sanhaleehana@naver.com>
2017-12-06 15:23:39 +0900

    minor fix

--- compiler/frontend/src/main/java/edu/snu/onyx/compiler/frontend/onyx/transform/transform/RelayTransform.java
+++ compiler/frontend/src/main/java/edu/snu/onyx/compiler/frontend/onyx/transform/transform/RelayTransform.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.snu.onyx.compiler.frontend.beam.transform;
+package edu.snu.onyx.compiler.frontend.onyx.transform.transform;
 
 import edu.snu.onyx.common.ir.OutputCollector;
 import edu.snu.onyx.common.ir.Transform;
