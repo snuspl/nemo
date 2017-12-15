bb6bd592c2fd5813b81764886f24590ebd75a8ce
DifferentSC <bert1234@snu.ac.kr>
2017-12-15 15:24:41 +0900

    Minor fix

--- webui/onyxwebserver/database.py
+++ webui/onyxwebserver/database.py
@@ -18,7 +18,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/vortex_web.db', convert_unicode=True)
+engine = create_engine('sqlite:////tmp/onyx_web.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
@@ -26,5 +26,5 @@ Base.query = db_session.query_property()
 
 
 def init_db():
-    import vortexwebserver.models
+    import onyxwebserver.models
     Base.metadata.create_all(bind=engine)
