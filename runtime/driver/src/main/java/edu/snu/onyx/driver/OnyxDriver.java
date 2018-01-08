16681330e3ec65f8b6655a0d4ece5f76255e0de6
Joo Yeon Kim <jykim88@gmail.com>
2018-01-08 16:29:10 +0900

    Checkstyle

--- runtime/driver/src/main/java/edu/snu/onyx/driver/OnyxDriver.java
+++ runtime/driver/src/main/java/edu/snu/onyx/driver/OnyxDriver.java
@@ -45,7 +45,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.LogManager;
