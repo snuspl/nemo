1dc3a88001ceace8a2e823959c99e63e1a177179
Jeongyoon Eo <jeongyoon0807@gmail.com>
2018-01-17 16:46:56 +0900

    minor fix

--- examples/beam/src/main/java/edu/snu/onyx/examples/beam/Broadcast.java
+++ examples/beam/src/main/java/edu/snu/onyx/examples/beam/Broadcast.java
@@ -29,7 +29,7 @@ import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 /**
- * Sample application that creates PCollectionView.
+ * Sample Broadcast application.
  */
 public final class Broadcast {
   /**
