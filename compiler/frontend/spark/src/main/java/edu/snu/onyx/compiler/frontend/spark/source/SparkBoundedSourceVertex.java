c34c1ae36a10d8fe102141c6c2c9ecee02346c0f
Won Wook SONG <wsong0512@gmail.com>
2018-01-25 17:35:10 +0900

    works

--- compiler/frontend/spark/src/main/java/edu/snu/onyx/compiler/frontend/spark/source/SparkBoundedSourceVertex.java
+++ compiler/frontend/spark/src/main/java/edu/snu/onyx/compiler/frontend/spark/source/SparkBoundedSourceVertex.java
@@ -5,11 +5,12 @@ import edu.snu.onyx.common.ir.ReadablesWrapper;
 import edu.snu.onyx.common.ir.vertex.SourceVertex;
 import edu.snu.onyx.compiler.frontend.spark.sql.Dataset;
 import org.apache.spark.Partition;
+import org.apache.spark.SparkConf;
 import org.apache.spark.TaskContext$;
+import org.apache.spark.rdd.RDD;
 import scala.collection.JavaConverters;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -17,7 +18,7 @@ import java.util.List;
  * @param <T> type of data to read.
  */
 public final class SparkBoundedSourceVertex<T> extends SourceVertex<T> {
-  private final List<Iterator<T>> partitionedData;
+  private final ReadablesWrapper<T> readablesWrapper;
 
   /**
    * Constructor.
@@ -25,46 +26,43 @@ public final class SparkBoundedSourceVertex<T> extends SourceVertex<T> {
    * @param dataset Dataset to read data from.
    */
   public SparkBoundedSourceVertex(final Dataset<T> dataset) {
-    this.partitionedData = new ArrayList<>();
-    for (final Partition partition : dataset.rdd().getPartitions()) {
-      final Iterator<T> data = JavaConverters
-          .asJavaIteratorConverter(dataset.rdd().compute(partition, TaskContext$.MODULE$.empty())).asJava();
-      partitionedData.add(data);
-    }
+    this.readablesWrapper = new SparkBoundedSourceReadablesWrapper(dataset);
   }
 
-  public SparkBoundedSourceVertex(final List<Iterator<T>> partitionedData) {
-    this.partitionedData = partitionedData;
+  public SparkBoundedSourceVertex(final ReadablesWrapper<T> readablesWrapper) {
+    this.readablesWrapper = readablesWrapper;
   }
 
   @Override
   public SparkBoundedSourceVertex getClone() {
-    final SparkBoundedSourceVertex<T> that = new SparkBoundedSourceVertex<>((this.partitionedData));
+    final SparkBoundedSourceVertex<T> that = new SparkBoundedSourceVertex<>((this.readablesWrapper));
     this.copyExecutionPropertiesTo(that);
     return that;
   }
 
   @Override
   public ReadablesWrapper<T> getReadableWrapper(final int desiredNumOfSplits) {
-    return new SparkBoundedSourceReadablesWrapper();
+    return readablesWrapper;
   }
 
   /**
    * A ReadablesWrapper for SparkBoundedSourceVertex.
    */
   private final class SparkBoundedSourceReadablesWrapper implements ReadablesWrapper<T> {
+    private final List<Readable<T>> readables;
+
     /**
      * Constructor.
      */
-    private SparkBoundedSourceReadablesWrapper() {
+    private SparkBoundedSourceReadablesWrapper(final Dataset<T> dataset) {
+      this.readables = new ArrayList<>();
+      for (final Partition partition: dataset.rdd().getPartitions()) {
+        readables.add(new BoundedSourceReadable(dataset.rdd().sparkContext().conf(), partition, dataset.rdd()));
+      }
     }
 
     @Override
     public List<Readable<T>> getReadables() {
-      final List<Readable<T>> readables = new ArrayList<>();
-      for (int i = 0; i < partitionedData.size(); i++) {
-        readables.add(new BoundedSourceReadable(i));
-      }
       return readables;
     }
   }
@@ -73,20 +71,19 @@ public final class SparkBoundedSourceVertex<T> extends SourceVertex<T> {
    * A Readable for SparkBoundedSourceReadablesWrapper.
    */
   private final class BoundedSourceReadable implements Readable<T> {
-    final Integer index;
+    final List<T> data;
 
     /**
      * Constructor.
-     * @param index
      */
-    private BoundedSourceReadable(final Integer index) {
-      this.index = index;
+    private BoundedSourceReadable(final SparkConf conf, final Partition partition, final RDD<T> rdd) {
+      this.data = new ArrayList<>();
+      JavaConverters.asJavaIteratorConverter(rdd.compute(partition, TaskContext$.MODULE$.empty())).asJava()
+          .forEachRemaining(data::add);
     }
 
     @Override
     public Iterable<T> read() {
-      final List<T> data = new ArrayList<>();
-      partitionedData.get(index).forEachRemaining(data::add);
       return data;
     }
   }
